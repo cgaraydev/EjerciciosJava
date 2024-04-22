d2b958451884e7a621f483c78f19d47d78eb2c40
Camilo Garay <cgaraydev@gmail.com>
2024-04-21 20:39:18 -0400

    Desarrollo

--- src/Basico.java
+++ src/Basico.java
@@ -5,45 +5,100 @@ public class Basico {
     public static void main(String[] args) {
 
 //        Declarar dos variables numericas, mostrar por consola el resultado de suma, resta, multiplicacion, division y modulo
-        double num1 = 11;
-        double num2 = 5;
-        System.out.println("Suma: " + (num1 + num2));
-        System.out.println("Resta: " + (num1 - num2));
-        System.out.println("Multiplicacion: " + (num1 * num2));
-        System.out.println("Division: " + (num1 / num2));
-        System.out.println("Modulo: " + (num1 % num2));
+//        double num1 = 11;
+//        double num2 = 5;
+//        System.out.println("Suma: " + (num1 + num2));
+//        System.out.println("Resta: " + (num1 - num2));
+//        System.out.println("Multiplicacion: " + (num1 * num2));
+//        System.out.println("Division: " + (num1 / num2));
+//        System.out.println("Modulo: " + (num1 % num2));
 
 //        Declarar dos variables numericas, indicar cual es el mayor o si son iguales
-        int num3 = 11;
-        int num4 = 14;
-        if (num3 > num4) {
-            System.out.println("El numero mayor es: " + num3);
-        } else if (num3 < num4) {
-            System.out.println("El numero mayor es: " + num4);
-        } else {
-            System.out.println("Los numeros son iguales");
-        }
+//        int num3 = 11;
+//        int num4 = 14;
+//        if (num3 > num4) {
+//            System.out.println("El numero mayor es: " + num3);
+//        } else if (num3 < num4) {
+//            System.out.println("El numero mayor es: " + num4);
+//        } else {
+//            System.out.println("Los numeros son iguales");
+//        }
 
 //        Declarar un string que contenga tu nombre y mostra un mensaje de bienvenida
-        String nombre = "";
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Introduce tu nombre: ");
-        nombre = sc.nextLine();
-        System.out.println("Hola " + nombre);
+//        String nombre = "";
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Introduce tu nombre: ");
+//        nombre = sc.nextLine();
+//        System.out.println("Hola " + nombre);
 
 //        Declarar un string que contenga tu nombre y mostra un mensaje de bienvenida, Utilizar JOptionPane
-        String nombreJOptionPane = JOptionPane.showInputDialog("Introduce tu nombre: ");
-        JOptionPane.showMessageDialog(null, "Bienvenido " + nombreJOptionPane);
+//        String nombreJOptionPane = JOptionPane.showInputDialog("Introduce tu nombre: ");
+//        JOptionPane.showMessageDialog(null, "Bienvenido " + nombreJOptionPane);
 
 //        Leer un numero por telcado e indicar si es par o impar
-        int numeroLeido = Integer.parseInt(JOptionPane.showInputDialog("Introduce un numero: "));
-        if (numeroLeido % 2 == 0) {
-            JOptionPane.showMessageDialog(null, "El numero es PAR");
-        } else {
-            JOptionPane.showMessageDialog(null, "El numero es IMPAR");
-        }
+//        int numeroLeido = Integer.parseInt(JOptionPane.showInputDialog("Introduce un numero: "));
+//        if (numeroLeido % 2 == 0) {
+//            JOptionPane.showMessageDialog(null, "El numero es PAR");
+//        } else {
+//            JOptionPane.showMessageDialog(null, "El numero es IMPAR");
+//        }
 
+//        Sumar desde el 1 hasta el numero que indiquemos
+//        int numeroParaSumar = Integer.parseInt(JOptionPane.showInputDialog("Introduce un numero: "));
+//        int suma = 0;
+//                for(int i = 1; i <= numeroParaSumar; i++){
+//            suma += i;
+//        }
+//        JOptionPane.showMessageDialog(null, "La suma es: " + suma);
 
+//        Leer un numero por teclado y mostrar su caracter en ASCII
+//        int numeroParaLeer = Integer.parseInt(JOptionPane.showInputDialog("Introduce un numero: "));
+//        char caracter = (char) numeroParaLeer;
+//        JOptionPane.showMessageDialog(null, "El caracter en ASCII es: " + caracter);
+
+//        Calcular sueldo de usuario, solicitar cantidad de horas y mostrar su sueldo a 15 dolares la hora
+//        int horas = Integer.parseInt(JOptionPane.showInputDialog("Introduce las horas: "));
+//        double sueldo = horas * 15;
+//        JOptionPane.showMessageDialog(null, "Su sueldo es: " + sueldo + " dolares");
+
+//        Leer un numero por teclado que pida el precio de un producto y calcule el precio con iva
+//        int numeroPedido = Integer.parseInt(JOptionPane.showInputDialog("Introduce el precio: "));
+//        double iva = (numeroPedido * 0.21) + numeroPedido;
+//        JOptionPane.showMessageDialog(null, "El precio con iva es: " + iva);
+
+//        Mostrar numeros 1 al 100 incluidos con while y luego con for
+//        int numero = 1;
+//        while(numero <= 100){
+//            System.out.println(numero);
+//            numero++;
+//        }
+//        for (int i = 1; i <= 100; i++) {
+//            System.out.println(i);
+//        }
+
+//        Mostrar numeros de 1 al 100 incluidos divisibles por 2 y 3
+//        for (int i = 1; i <= 100; i++) {
+//            if(i % 2 == 0 || i % 3 == 0){
+//                System.out.println(i);
+//            }
+//        }
+
+//        Aplicacion q pida un numero de ventas y luego pida las ventas por teclado, al final sumar todas las ventas
+//        int numeroVentas = Integer.parseInt(JOptionPane.showInputDialog("Introduce el numero de ventas: "));
+//        double sumaVentas = 0;
+//        for (int i = 1; i <= numeroVentas; i++) {
+//            double venta = Double.parseDouble(JOptionPane.showInputDialog("Introduce la venta: "));
+//            sumaVentas += venta;
+//        }
+//        JOptionPane.showMessageDialog(null, "La suma de las ventas es: " + sumaVentas);
+
+//        Leer numero por telcado, comprobar q sea mayor o igual a cero, si no lo es vuelve a pedir el numero usando do while
+//        luego mostrar numero por pantalla
+//        double numero;
+//        do {
+//            numero = Double.parseDouble(JOptionPane.showInputDialog("Introduce un numero mayor o igual a cero: "));
+//        } while (numero < 0);
+//        System.out.println("El numero es: " + numero);
 
 
     }
