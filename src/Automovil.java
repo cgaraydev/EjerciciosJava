c476185346797b3e7a0a545abac186454f1d048e
Camilo Garay <cgaraydev@gmail.com>
2024-04-22 13:31:01 -0400

    Desarrollo

---
+++ src/Automovil.java
@@ -0,0 +1,57 @@
+public class Automovil {
+    String marca;
+    String color;
+    int potencia;
+    double precio;
+
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getPotencia() {
+        return potencia;
+    }
+
+    public void setPotencia(int potencia) {
+        this.potencia = potencia;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public void mostrarInformacion() {
+        System.out.println("La marca es: " + marca);
+        System.out.println("El color es: " + color);
+        System.out.println("La potencia es: " + potencia);
+        System.out.println("El precio es: " + precio);
+    }
+
+    @Override
+    public String toString() {
+        return "Automovil{" +
+                "marca='" + marca + '\'' +
+                ", color='" + color + '\'' +
+                ", potencia=" + potencia +
+                ", precio=" + precio +
+                '}';
+    }
+}
+
--- src/Intermedio.java
+++ src/Intermedio.java
@@ -1,2 +1,48 @@
+import javax.swing.*;
+import java.util.Scanner;
+
 public class Intermedio {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+//        Declarar App con una clase Automovil con las propiedades marca, color, potencia y precio, declarar getters y
+//        setters y un metodo para visualizar la info
+//        Automovil automovil1 = new Automovil();
+//        Automovil automovil2 = new Automovil();
+//
+//        automovil1.setMarca("Toyota");
+//        automovil1.setColor("Rojo");
+//        automovil1.setPotencia(180);
+//        automovil1.setPrecio(200000);
+//        automovil2.setMarca("Nissan");
+//        automovil2.setColor("Azul");
+//        automovil2.setPotencia(200);
+//        automovil2.setPrecio(250000);
+//
+//        automovil1.mostrarInformacion();
+//        automovil2.mostrarInformacion();
+
+//        Solicitar precio, aplicar iva y mostrar por pantalla el precio final, crear diferentes metodos
+//        ingresarPrecio();
+    }
+
+//    public static void ingresarPrecio() {
+//        double precio = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el precio"));
+//        mostrarResultado(calcularPrecioFinal(precio));
+//    }
+//
+//    public static double calcularPrecioFinal(double precio) {
+//        double precioFinal = precio * 1.21;
+//        return precioFinal;
+//    }
+//
+//    public static void mostrarResultado(double precioFinal) {
+//        JOptionPane.showMessageDialog(null, "El precio final es: " + precioFinal);
+//    }
+
+
+
+
 }
